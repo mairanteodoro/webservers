e2ab3568fa9d0a5bafed8c5adf44eca6027f04e2
Mairan Teodoro <mteodoro@stansberryresearch.com>
2017-12-21 09:13:35 -0500

    Added comments.

--- server_maria/server.js
+++ server_maria/server.js
@@ -3,8 +3,14 @@
 var http = require('http');
 var pathUtils = require('path');
 var express = require('express');
+
 var app = express();
+
+// choose wisely
 var PORT = process.env.PORT || 3000;
+
+// the server can be started from anywhere
+// relative to the static files dir
 var appDir = pathUtils.resolve(__dirname, staticFilesDir);
 
 // serving static files
@@ -16,7 +22,7 @@ app.get('*', function(req, res) {
     res.sendFile(pathUtils.resolve(appDir, 'index.html'));
 });
 
-// create server
+// create the server and listen on PORT
 http.createServer(app).listen(PORT, function() {
     console.log('Express server listening on port ' + PORT);
     console.log('http://localhost:' + PORT);
